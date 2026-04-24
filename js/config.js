c25e393950e0adc422eab70e7454422612d0e504
imyulyarman-eng <im.yulyarman@gmail.com>
2026-04-24 18:34:59 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,14 +1,8 @@
 const CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbwlvZJAnrLlmzvKN9ArL1C3WOSGXXl6cjRIf7et_iQGpXVjeexnIpeV_zSUZItjtvlZKw/exec",
-  LOGIN_PIN: "2512", // PIN 4 digit default
-  SESSION_TIMEOUT: 30 * 60 * 1000, // 30 menit
-  HARGA: {
-    kambing: 4500000,
-    sapi: 30000000,
-  },
-  KELAS_LIST: [
-    "1A","1B","1C","2A","2B","2C","3A","3B","3C",
-    "4A","4B","4C","5A","5B","5C","6A","6B","6C",
-  ],
-  METODE_PEMBAYARAN: ["Tunai", "Transfer", "QRIS"],
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxCJg30_81ISi10dOFbvfUlezPFSprxBNsAbb0ThXsjlhAu3q5KwD7c7EiD_r4zbqcTtg/exec",
+  LOGIN_PIN: "2512",
+  SESSION_TIMEOUT: 30 * 60 * 1000,
+  HARGA: { kambing: 4500000, sapi: 30000000 },
+  KELAS_LIST: ["1A","1B","1C","2A","2B","2C","3A","3B","3C","4A","4B","4C","5A","5B","5C","6A","6B","6C"],
+  METODE_PEMBAYARAN: ["Tunai", "Transfer", "QRIS"]
 };
